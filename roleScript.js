53159f80a4bb1d543e3cb2808578bcea465c169f
riffhi <bhanushaliriddhi2205@gmail.com>
2025-08-04 02:23:09 +0530

    customized

---
+++ roleScript.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "an AI/ML Engineer",
+      "a Full Stack Developer",
+      "2x Ideathon Winner",
+      "4x Hackathon Finalist",
+      "Riddhi Bhanushali",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
